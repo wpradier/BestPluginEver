56eeb0e2abfb82b8a2b89be9e745d4fa2b03b88d
wpradier <wpradier@student.42.fr>
2019-03-05 20:15:07 +0100

    installing script

--- install.sh
+++ install.sh
@@ -3,9 +3,24 @@ echo 'kekette()
  {
          if [ "$1" = "rouge" ]                   
          then
-                 sh ~/.BePlEv/drawings/aouiouiR
-         else
-                 sh ~/.BePlEv/drawings/aouioui
+                 sh ~/.BePlEv/drawings/aouiouiR.sh
+	 elif [ "$1" = "verte" ]
+	 then
+		 sh ~/.BePlEv/drawings/aouiouiV.sh
+	 elif [ "$1" = "jaune" ]
+	 then
+		 sh ~/.BePlEv/drawings/aouiouiJ.sh
+	 elif [ "$1" = "bleue" ]
+	 then
+		 sh ~/.BePlEv/drawings/aouiouiB.sh
+	 elif [ "$1" = "rose" ]
+	 then
+		 sh ~/.BePlEv/drawings/aouiouiRO.sh
+	 elif [ "$1" = "cyan" ]
+	 then
+		 sh ~/.BePlEv/drawings/aouiouiC.sh
+	 else
+		 sh ~/.BePlEv/drawings/aouioui.sh
 	 fi
  
  }
