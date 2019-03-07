0408b42a6030ed1c20ffc05c32ecccbd2215b175
wpradier <wpradier@student.42.fr>
2019-03-07 23:09:27 +0100

    uninstalling script

--- uninstall.sh
+++ uninstall.sh
@@ -1,2 +1,10 @@
-#!/bin/zsh
-sed -i '.bak' /"alias kekette"/d ~/.zshrc && rm -rf ~/.BePlEv
+#!/bin/bash
+
+local VERSION=""
+
+if [ "$SHELL" == "/bin/zsh" ]; then
+	VERSION="~/.zshrc"
+elif [ "$SHELL" == "/bin/bash" ]; then
+	VERSION="~/.bashrc"
+
+sed -i '.bak' /"alias kekette"/d $VERSION && rm -rf ~/.BePlEv
