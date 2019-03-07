831b90c650d93207bde677d427ce59e30f52faa2
wpradier <wpradier@student.42.fr>
2019-03-07 23:51:26 +0100

    uninstalling script

--- uninstall.sh
+++ uninstall.sh
@@ -6,5 +6,6 @@ if [ -n `echo $SHELL | grep zsh` ]; then
 	VERSION_="$HOME/.zshrc"
 elif [ -n `echo $SHELL | grep bash` ]; then
 	VERSION_="$HOME/.bashrc"
+fi
 
 sed -i '.bak' /"alias kekette"/d $VERSION_ && rm -rf ~/.BePlEv
