3f5663922b1700eeef59ca7cef7a741d33c82c13
wpradier <wpradier@student.42.fr>
2019-03-07 23:08:54 +0100

    installing script

--- install.sh
+++ install.sh
@@ -1,2 +1,14 @@
-#!/bin/zsh
-echo 'alias kekette="~/.BePlEv/aouiouiX.sh"' >> ~/.zshrc
+#!/bin/bash
+
+local VERSION=""
+
+if [ "$SHELL" == "/bin/zsh" ]; then
+	VERSION="~/.zshrc"
+elif [ "$SHELL" == "/bin/bash" ]; then
+	VERSION="~/.bashrc"
+else
+	printf "\033[31m ERROR: Unsupported shell.\n\033[0mAvailable shells for the Best Plugin Ever to work:\n\033[30;47mBASH\nZSH\nUninstalling..."
+	~/.BePlEv/uninstall.sh
+	exit
+fi
+echo 'alias kekette="~/.BePlEv/aouiouiX.sh"' >> $VERSION
