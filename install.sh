26be4effaac2e43dcfef02769e434e2e7d5e73fb
Thomas Ubuntu <thomas.lddv@orange.fr>
2019-03-07 23:39:02 +0100

    install correction

--- install.sh
+++ install.sh
@@ -2,10 +2,10 @@
 
 VERSION_=""
 
-if [ "$SHELL" == "/bin/zsh" ]; then
-	VERSION_="~/.zshrc"
-elif [ "$SHELL" == "/bin/bash" ]; then
-	VERSION_="~/.bashrc"
+if [ -n `echo $SHELL | grep zsh` ]; then
+	VERSION_="$HOME/.zshrc"
+elif [ -n `echo $SHELL | grep bash` ]; then
+	VERSION_="$HOME/.bashrc"
 else
 	printf "\033[31m ERROR: Unsupported shell.\n\033[0mAvailable shells for the Best Plugin Ever to work:\n\033[30;47mBASH\nZSH\nUninstalling..."
 	~/.BePlEv/uninstall.sh
